cfa79aa95b9a1ba90b8878f4e6f6ba8396283043
rusprus <kazanpiter@gmail.com>
2022-01-31 17:22:53 +0300

    Добавил потоки

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+#include <thread>
 #include <iostream>
+#include <string>
 #include <stdio.h>
+#include <chrono>
 
 using namespace std;
 
-int main()
-{
 int ba(0);
 int y(0);
 int oldY(0);
@@ -14,6 +15,76 @@ bool state(false);
 bool timerOn(false);
 long startTime(0);
 
+void timerProc() {
+    while (y!=255) {
+
+      if(timerOn){
+
+          if( ( time(0) - startTime ) > rdv  ){
+              state = !state;
+              timerOn = false;
+              cout<<endl;
+              cout<<"Значение выше ВА: "<< state << endl;
+              cout<<"Время срабатывания: "<< time(0) -  startTime   << endl;
+          }
+      }
+      std::this_thread::sleep_for(std::chrono::nanoseconds(1000000000));
+   }
+}
+
+void mainProc(){
+    while(true){
+
+        if( y==255 )return ;
+
+        cout << "Входное значение Y: ";
+        oldY = y;
+        cin >> y ;
+
+        // 1 случай: предыдущий Y ниже уставки
+        if(!state){
+            // Текущий сигнал выше уставки
+
+             if( (y > ba) && !timerOn ){
+                 timerOn = true;
+                 startTime = time(0);
+                 cout<<"-->Первый случай: "<< endl;
+             }
+        }
+
+        // 2 случай: предыдущий Y выше уставки + RMV
+        if(state  && ( oldY > ba+rmv )){
+            // Текущий сигнал выше уставки.
+             if( y > ba  ){
+                // Ничего не делаем
+             }
+             // Текущий сигнал ниже уставки.
+             if( (y < ba) && !timerOn){
+                timerOn = true;
+                startTime = time(0);
+                cout<<"-->Второй случай: "<< endl;
+             }
+        }
+
+        // 3 случай: предыдущий Y выше уставки,но ниже (уставка + RMV)
+        if(state  && ( oldY < ba+rmv ) && ( oldY > ba )){
+            // Текущий сигнал выше уставки.
+             if( y > ba  ){
+                // Ничего не делаем
+             }
+             // Текущий сигнал ниже уставки.
+
+             if( (y < ba - rmv) && !timerOn){
+                timerOn = true;
+                startTime = time(0);
+                cout<<"-->Третий случай: "<< endl;
+             }
+        }
+    }
+}
+
+int main()
+{
 
 cout<<"255 для выхода "<< endl;
 cout << "Введите уставку ВА: ";
@@ -24,64 +95,10 @@ cout << "Введите отклонение от уставки RMV: ";
 cin >> rmv;
 startTime = time(0);
 
-while(true){
+std::thread mainThread(mainProc);
+std::thread timerThread(timerProc);
+timerThread.join();
+mainThread.join();
 
-    if( y==255 )return 0;
-
-    cout << "Входное значение Y: ";
-
-    oldY = y;
-    cin >> y ;
-
-    if(timerOn){
-
-        cout<<"delta time: "<< time(0) -  startTime   << endl;
-        if( ( time(0) - startTime ) > rdv  ){
-            state = !state;
-        }
-    }
-
-    // 1 случай: предыдущий Y ниже уставки
-    if(!state){
-        // Текущий сигнал выше уставки
-         if( (y > ba) && !timerOn ){
-             timerOn = true;
-             startTime = time(0);
-         }
-         // Текущий сигнал ниже уставки.
-         if(y < ba){
-            timerOn = false;
-         }
-    }
-
-    // 2 случай: предыдущий Y выше уставки + RMV
-    if(state  && ( oldY > ba+rmv )){
-        // Текущий сигнал выше уставки.
-         if( y > ba  ){
-             timerOn = false;
-         }
-         // Текущий сигнал ниже уставки.
-         if( (y < ba) && !timerOn){
-            timerOn = true;
-            startTime = time(0);
-         }
-    }
-
-    // 3 случай: предыдущий Y выше уставки,но ниже (уставка + RMV)
-    if(state  && ( oldY < ba+rmv ) && ( oldY > ba )){
-        // Текущий сигнал выше уставки.
-         if( y > ba  ){
-             timerOn = false;
-         }
-         // Текущий сигнал ниже уставки.
-         if( (y < ba) && !timerOn){
-            timerOn = true;
-            startTime = time(0);
-         }
-    }
-
-    cout<<"Значение выше ВА: "<< state << endl;
-
-}
 return 0;
 }
